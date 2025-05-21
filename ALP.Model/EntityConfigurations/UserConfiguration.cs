4083680d92fb8a958b827b23e526023469820bd9
GogiGunia <gogigunia@gmail.com>
2025-05-21 16:38:22 +0200

    ALP Dev: Aktualisieren des User Models zur Vorbereitung der Implementierung der TokenService.

--- ALP.Model/EntityConfigurations/UserConfiguration.cs
+++ ALP.Model/EntityConfigurations/UserConfiguration.cs
@@ -12,7 +12,11 @@ namespace ALP.Model.EntityConfigurations
             builder.ToTable("Users");
 
             builder.Property(e => e.Email).IsRequired().HasMaxLength(255);
+            builder.Property(e => e.FirstName).HasMaxLength(100).IsRequired(true);
+            builder.Property(e => e.LastName).HasMaxLength(100).IsRequired(true);
             builder.Property(e => e.PasswordHash).IsRequired().HasMaxLength(512);
+            builder.Property(e => e.PasswordResetToken).HasMaxLength(255).IsRequired(false);
+            builder.Property(e => e.PasswordResetTokenExpiry).IsRequired(false);
             builder.Property(e => e.Role).IsRequired().HasMaxLength(20).HasConversion<string>();
             builder.Property(e => e.CreatedAt).IsRequired().HasColumnType("datetime2").HasDefaultValueSql("GETUTCDATE()");
             builder.HasIndex(e => e.Email).IsUnique().HasDatabaseName("UQ_Users_Email");
